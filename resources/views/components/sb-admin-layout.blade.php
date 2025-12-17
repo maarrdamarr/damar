5564df5600b7b161ffa06ad3cedbe305daa45717
maarrdamarr <damardarma42@gmail.com>
2025-12-17 21:55:55 +0700

    tulisan-sidebar

--- resources/views/components/sb-admin-layout.blade.php
+++ resources/views/components/sb-admin-layout.blade.php
@@ -11,6 +11,11 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link href="https://startbootstrap.github.io/startbootstrap-sb-admin-2/css/sb-admin-2.min.css" rel="stylesheet">
+    <style>
+        .sidebar-brand .brand-kuno {
+            color: #d1a846;
+        }
+    </style>
 </head>
 
 <body id="page-top">
@@ -23,7 +28,9 @@
                 <div class="sidebar-brand-icon">
                     <img src="{{ asset('images/logo-a.svg') }}" alt="A" style="width:40px;height:40px;">
                 </div>
-                <div class="sidebar-brand-text mx-3">ARTKUNO</div>
+                <div class="sidebar-brand-text mx-3">
+                    <span class="brand-art">ART</span><span class="brand-kuno">KUNO</span>
+                </div>
             </a>
 
             <hr class="sidebar-divider my-0">
